be283be2a10ca7557c39938ea2437d605997757a
Julia Caetano <caetanojuliaduarte@gmail.com>
2026-08-16 21:15:23 +0000

    build cracha02

--- docs/cracha02/flutter_bootstrap.js
+++ docs/cracha02/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1625959002" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "303635841" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
